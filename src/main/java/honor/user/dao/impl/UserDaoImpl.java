1cedc63682b66ce4480c8fa8e1b060432f539037
huiyang89 <chenhuiyang89@126.com>
2015-07-01 11:35:54 +0800

    pom modify

--- src/main/java/honor/user/dao/impl/UserDaoImpl.java
+++ src/main/java/honor/user/dao/impl/UserDaoImpl.java
@@ -5,7 +5,6 @@ import honor.user.dao.UserDao;
 
 public class UserDaoImpl implements UserDao{
 
-	@Override
 	public User getUserById(String id) {
 		// TODO Auto-generated method stub
 		return null;
